aa18f319c5ce9faebe5274697e049b674a9015c9
RigHakiko <add-122@hotmail.com>
2022-10-05 21:39:24 +0900

    Initial commit

---
+++ src/main/java/com/group6/mp2/Day08Pm20221005InClassApplication.java
@@ -0,0 +1,13 @@
+package com.group6.mp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day08Pm20221005InClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day08Pm20221005InClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/mp2/Day08Pm20221005InClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.mp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day08Pm20221005InClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
