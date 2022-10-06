e14cfa462a752fb6161779be38ee30045fff968b
RigHakiko <add-122@hotmail.com>
2022-10-06 20:28:59 +0900

    成功写完

--- src/main/java/com/group6/mp2/Day08Pm20221005InClassApplication.java
+++ src/main/java/com/group6/mp2/Day08Pm20221005InClassApplication.java
@@ -1,13 +1,15 @@
 package com.group6.mp2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.group6.mp2.mapper")
 public class Day08Pm20221005InClassApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day08Pm20221005InClassApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Day08Pm20221005InClassApplication.class, args);
+    }
 
 }
--- src/test/java/com/group6/mp2/Day08Pm20221005InClassApplicationTests.java
+++ src/test/java/com/group6/mp2/Day08Pm20221005InClassApplicationTests.java
@@ -1,13 +1,25 @@
 package com.group6.mp2;
 
+import com.group6.mp2.mapper.BookMapper;
+import com.group6.mp2.service.IBookService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Day08Pm20221005InClassApplicationTests {
+    @Autowired
+    private IBookService iBookService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test20221006(){
+
+        iBookService.buy("tom", 1);
+
+    }
 
 }
